4613b13b0f39b767e40672069e907fd229979d78
Daas Houssem <houssemdaas2@gmail.com>
2025-04-24 15:20:31 +0100

    feat: add new dependencies and enhance global styles with interactive components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { Link as ScrollLink, Element } from "react-scroll";
+
 import Image from "next/image";
 import Link from "next/link";
+import { InteractiveHoverButton } from "@/components/magicui/interactive-hover-button";
 
 export default function Home() {
   return (
@@ -10,7 +12,7 @@ export default function Home() {
         <Element
           name="top"
           className="overflow-hidden rounded-[6px] top-5 sticky md:mx-auto z-50 xl:w-4/5 2xl:w-[68%]
-      bg-lochmara-200 flex  items-center justify-between py-6 px-4 md:px-8 mx-6"
+      bg-lochmara-200 flex  items-center justify-between py-4 px-4 md:px-8 mx-6"
         >
           <Link href="/">
             <Image
@@ -18,9 +20,50 @@ export default function Home() {
               alt="Navix"
               width={1000}
               height={1000}
-              className="w-20"
+              className="w-28"
             />
           </Link>
+          <div className="absolute right-1/2 translate-x-1/2 transform">
+            <div className="hidden md:flex gap-x-10 items-center text-gray-700 font-medium text-lg cursor-pointer">
+              <Link href="/#showcase" className="hover:text-lochmara-500">
+                Showcase
+              </Link>
+              <ScrollLink
+                to="process"
+                smooth={true}
+                duration={500}
+                className="hover:text-lochmara-500"
+              >
+                Process
+              </ScrollLink>
+              <ScrollLink
+                to="services"
+                smooth={true}
+                duration={500}
+                className="hover:text-lochmara-500"
+              >
+                Services
+              </ScrollLink>
+              <ScrollLink
+                to="guarentees"
+                smooth={true}
+                duration={500}
+                className="hover:text-lochmara-500"
+              >
+                Guarentees
+              </ScrollLink>
+            </div>
+          </div>
+          <div className="flex items-center gap-4">
+            <div className="hover:text-lochmara-500">
+           <a href="tel:50699724" className="hidden lg:flex">
+            <button className="px-4 py-2 flex items-center gap-x-3  rounded-md">
+ (+216) 50 699 724
+            </button>
+           </a>
+           <InteractiveHoverButton className="">Hover Me</InteractiveHoverButton>
+            </div>
+          </div>
         </Element>
       </div>
     </div>

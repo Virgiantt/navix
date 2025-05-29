93e6c5edbc73be24fa26ccb39a785b34d7504be4
Houssem Daas <houssemdaas2@gmail.com>
2025-05-29 21:43:07 +0100

    feat: add project schema type to Sanity and update schema definition

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { categoryType } from './categoryType'
 import {clientType} from './clientType'
+import { projectType } from './ProjectType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [categoryType,clientType],
+  types: [categoryType,clientType,projectType],
 }
